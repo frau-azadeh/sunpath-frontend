e7f7dac7de4c6975ecab8034b71edc323a0bb0fc
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2026-08-11 22:14:44 +0330

    update prettier

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,10 @@
-import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
-import './globals.css';
+
 // حتماً این خط رو اضافه کن 👇
 import 'leaflet/dist/leaflet.css';
+import type { Metadata } from 'next';
+
+import './globals.css';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
