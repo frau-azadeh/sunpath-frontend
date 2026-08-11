61bfe101acc96807e40f68d76e99a964f7d97d34
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2026-08-11 23:57:25 +0330

    update show car and prettier

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import { Geist, Geist_Mono } from 'next/font/google';
 import Script from 'next/script';
-import type { Metadata } from 'next';
 
 import 'leaflet/dist/leaflet.css';
+import type { Metadata } from 'next';
+
 import './globals.css';
 
 const geistSans = Geist({
@@ -32,15 +33,10 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <head>
-        <Script
-          src="/config/config.js"
-          strategy="beforeInteractive"
-        />
+        <Script src="/config/config.js" strategy="beforeInteractive" />
       </head>
 
-      <body className="flex h-full flex-col">
-        {children}
-      </body>
+      <body className="flex h-full flex-col">{children}</body>
     </html>
   );
 }
