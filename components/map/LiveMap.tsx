c8df8370f7474584626e9ba210043bf371a831dd
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2026-08-11 22:15:04 +0330

    update prettier

--- components/map/LiveMap.tsx
+++ components/map/LiveMap.tsx
@@ -1,16 +1,20 @@
 'use client';
 
 import { useEffect } from 'react';
-import { MapContainer, TileLayer, useMap } from 'react-leaflet';
+
 import L from 'leaflet';
+import { MapContainer, TileLayer, useMap } from 'react-leaflet';
+
 import { useVehicleStore } from '@/store/useVehicleStore';
+
 import VehicleMarker from './VehicleMarker';
 
 // رفع مشکل بارگذاری آیکون پیش‌فرض در Next.js
 if (typeof window !== 'undefined') {
   delete (L.Icon.Default.prototype as any)._getIconUrl;
   L.Icon.Default.mergeOptions({
-    iconRetinaUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
+    iconRetinaUrl:
+      'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
     iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
     shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
   });
@@ -53,12 +57,14 @@ export default function LiveMap() {
         <MapResizeHandler />
 
         <TileLayer
-          attribution='&copy; OpenStreetMap contributors'
+          attribution="&copy; OpenStreetMap contributors"
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
 
         {vehicles
-          .filter((vehicle) => vehicle.latitude != null && vehicle.longitude != null)
+          .filter(
+            (vehicle) => vehicle.latitude != null && vehicle.longitude != null,
+          )
           .map((vehicle) => (
             <VehicleMarker key={vehicle.id} vehicle={vehicle} />
           ))}
