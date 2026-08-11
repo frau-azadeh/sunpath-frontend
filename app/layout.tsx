b805929527c65b17916a4bc195b9c5db6b913a39
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2026-08-11 23:55:24 +0330

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,8 @@
 import { Geist, Geist_Mono } from 'next/font/google';
-
-// حتماً این خط رو اضافه کن 👇
-import 'leaflet/dist/leaflet.css';
+import Script from 'next/script';
 import type { Metadata } from 'next';
 
+import 'leaflet/dist/leaflet.css';
 import './globals.css';
 
 const geistSans = Geist({
@@ -32,7 +31,16 @@ export default function RootLayout({
       dir="rtl"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="h-full flex flex-col">{children}</body>
+      <head>
+        <Script
+          src="/config/config.js"
+          strategy="beforeInteractive"
+        />
+      </head>
+
+      <body className="flex h-full flex-col">
+        {children}
+      </body>
     </html>
   );
 }
