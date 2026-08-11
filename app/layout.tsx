23a92366f36bf9cf3ac801ef35c40dba9f354732
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2026-08-11 22:13:27 +0330

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
-import { Geist, Geist_Mono } from 'next/font/google';
-
 import type { Metadata } from 'next';
-
+import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
+// حتماً این خط رو اضافه کن 👇
+import 'leaflet/dist/leaflet.css';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -15,17 +15,22 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: 'Sun Path',
-  description: 'Sun Path APP',
+  title: 'SunPath',
+  description: 'سیستم مدیریت ناوگان SunPath',
 };
 
-export default function RootLayout({ children }: LayoutProps<'/'>) {
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
     <html
-      lang="en"
+      lang="fa"
+      dir="rtl"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="h-full flex flex-col">{children}</body>
     </html>
   );
 }
