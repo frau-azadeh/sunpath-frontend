101c2f9688e485f86fb5589b16070745bfb74fef
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2026-08-12 19:25:52 +0330

    update ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,9 @@
-import { Geist, Geist_Mono } from 'next/font/google';
-import Script from 'next/script';
-
-import 'leaflet/dist/leaflet.css';
 import type { Metadata } from 'next';
+import Script from 'next/script';
+import Providers from './providers';
 
+import '@fontsource/vazirmatn/index.css';
 import './globals.css';
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
 
 export const metadata: Metadata = {
   title: 'SunPath',
@@ -23,20 +12,17 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <html
-      lang="fa"
-      dir="rtl"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
+    <html lang="fa" dir="rtl" suppressHydrationWarning>
       <head>
-        <Script src="/config/config.js" strategy="beforeInteractive" />
+        <Script src="/config/config.js"  />
       </head>
-
-      <body className="flex h-full flex-col">{children}</body>
+      <body className="font-vazir antialiased">
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
