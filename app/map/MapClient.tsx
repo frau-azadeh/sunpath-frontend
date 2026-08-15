cd67d3395f4895f90c8d75276ebbfae2cad453bf
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2026-08-15 20:15:15 +0330

    update error build

---
+++ app/map/MapClient.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import dynamic from 'next/dynamic';
+
+const LiveMapEnhanced = dynamic(
+  () => import('@/components/map/LiveMapEnhanced'),
+  {
+    ssr: false,
+    loading: () => (
+      <div className="h-full w-full animate-pulse rounded-[28px] bg-slate-100 dark:bg-slate-800" />
+    ),
+  }
+);
+
+export default function MapClient() {
+  return <LiveMapEnhanced />;
+}
