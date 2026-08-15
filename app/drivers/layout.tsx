e68f7daea3716b3f32cc613cfa32ab3e54d147ed
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2026-08-15 19:20:52 +0330

    update

---
+++ app/drivers/layout.tsx
@@ -0,0 +1,13 @@
+import type { ReactNode } from 'react';
+
+import { DashboardLayout } from '@/components/layout/DashboardLayout';
+
+interface DriversLayoutProps {
+  children: ReactNode;
+}
+
+export default function DriversLayout({
+  children,
+}: DriversLayoutProps) {
+  return <DashboardLayout>{children}</DashboardLayout>;
+}
